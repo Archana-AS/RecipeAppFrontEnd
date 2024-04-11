e4aace3cae7bd94a62104389e951f373813f7d20
Archana-AS <archanaas2321@gmail.com>
2024-04-11 11:59:41 +0530

    Ädd Recipe completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import AddRecipe from './components/AddRecipe';
 
 function App() {
   return (
     <div >
-      <p1>hello</p1>
-      
+    
+  <AddRecipe/>
+  
     </div>
   );
 }
