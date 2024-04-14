07d7c5bb1b7efce656cf00dc59d14d9afb7434eb
Archana-AS <archanaas2321@gmail.com>
2024-04-14 19:17:16 +0530

    Project completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './components/AddRecipe';
 import SearchRecipe from './components/SearchRecipe';
+import ViewRecipe from './components/ViewRecipe';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-    
-  <AddRecipe/>
-  <SearchRecipe/>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path ='/' element={<AddRecipe/>} />
+          <Route path ='/Search' element={<SearchRecipe/>}/>
+          <Route path ='/View' element={<ViewRecipe/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/ViewRecipe.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewRecipe = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img height="300" src="https://th.bing.com/th/id/OIP.u7fXe5v5K2lS0pPzbx9GpAHaE3?w=768&h=505&rs=1&pid=ImgDetMain" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Braised pork belly</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height="300" src="https://th.bing.com/th/id/OIP.GkovrXqJV1kEl27BgaWs3gHaHa?rs=1&pid=ImgDetMain" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Beef Ribs</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height="300" src="https://venudindigulbiriyani.sg/wp-content/uploads/2023/06/Chilli-Chicken.jpg" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Chilli Chiken</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height="300" src="https://th.bing.com/th/id/R.8624ef8f49565dde0c2e0fb54948ceaa?rik=D1Q%2fQmdvZB1tkg&riu=http%3a%2f%2fwww.enkivillage.com%2fs%2fupload%2fimages%2f2014%2f12%2facb06f7149cb77b42e8a8d8c12c410cc.jpg&ehk=4BGaExUELBKdFKpFhVqWSL%2bZoWWA7jVtYAdR2paZoc0%3d&risl=&pid=ImgRaw&r=0" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Sushi</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height="300" src="https://cdn.shortpixel.ai/client/q_lossy,ret_img,w_2365,h_3112/https://tatyanaseverydayfood.com/wp-content/uploads/2016/02/Garlic-Chicken-Salad.jpg" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Garlic chicken Salad</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height="300" src="https://wallpapercave.com/wp/wp2115243.jpg" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Steak</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height="300" src="https://th.bing.com/th/id/OIP.sXwi_RheGzSRYAdT8PZMJwHaGI?rs=1&pid=ImgDetMain" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Croissant</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height="300" src="https://www.licious.in/blog/wp-content/uploads/2022/07/shutterstock_1582779079.jpg" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Shrimp Fried rice</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height="300" src="https://insanelygoodrecipes.com/wp-content/uploads/2021/07/Homemade-Chocolate-Shake-with-Cherry-and-Whipped-Cream.jpg" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Chocolate shake</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img height="300" src="https://im1.dineout.co.in/images/uploads/restaurant/sharpen/1/z/j/p106440-1653473509628e00e5f1748.jpg" class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Mocktail</h5>
+                                        <p class="card-text">ingredients required</p>
+                                        <a href="#" class="btn btn-primary">Recipe</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewRecipe
